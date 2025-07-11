80170266d74c7127566eb002e025dcfa615f8689
polynomeer <polynomeer@gmail.com>
2025-07-11 14:06:13 +0900

    build: initialize the project

---
+++ src/test/java/com/polynomeer/romanticker/RomantickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polynomeer.romanticker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomantickerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
